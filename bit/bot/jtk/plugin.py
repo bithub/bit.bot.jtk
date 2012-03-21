263c227c77b1eba6477c853af131de9640d65bc2
Ryan Northey <ryan@3ca.org.uk>
2012-03-21 18:53:38 +0000

    removed legacy code

--- bit/bot/jtk/plugin.py
+++
@@ -1,9 +0,0 @@
-from zope.interface import implements
-from zope.component import getUtility
-
-from bit.core.interfaces import IPlugin
-from bit.bot.common.interfaces import IResourceRegistry
-from bit.bot.base.plugin import BotPlugin
-
-
-
