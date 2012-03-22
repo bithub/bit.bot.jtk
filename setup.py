7e4af6f63535c5e234411520f6c0a5c696b4b31d
Ryan Northey <ryan@3ca.org.uk>
2012-03-22 11:12:54 +0000

    pep8 improvement

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup, find_packages
 import os
 
+
 version = '0.0.6'
+
 
 setup(name='bit.bot.jtk',
       version=version,
       description="Bit Bot Jtk",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
